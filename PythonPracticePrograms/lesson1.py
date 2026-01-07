c248dfa41bd3eb960a28bea15680d559ae5a5cec
josh-k25 <joshuadanielkung@gmail.com>
2026-01-07 15:42:46 -0500

    Created python program repo

---
+++ PythonPracticePrograms/lesson1.py
@@ -0,0 +1,4 @@
+name = "Josh"
+year = 2026
+birth_year = year - 18
+print(f"{name} was born in {birth_year}")
---
+++ PythonPracticePrograms/lesson4.py
@@ -0,0 +1,5 @@
+nums = [3, 6, 9, 12]
+nums.append(15)
+nums.remove(6)
+for n in nums:
+    print(n * 21)
